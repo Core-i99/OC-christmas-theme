f9691b05ea3fef1593f15d7c284944c932bcd3b2
Stijn Rombouts <62154074+Core-i99@users.noreply.github.com>
2022-12-23 18:44:16 +0100

    Add Workflow

---
+++ Build/Build.py
@@ -0,0 +1,13 @@
+import os
+import shutil
+
+if (os.path.exists("./Build/Output")):
+    shutil.rmtree("./Build/Output")
+
+os.mkdir("./Build/Output")
+os.mkdir("./Build/Output/Temp")
+os.mkdir("./Build/Output/Temp/Core-i99")
+
+shutil.copytree("./Export/icns", "./Build/Output/Temp/Core-i99/Christmas/")
+
+shutil.make_archive("./Build/Output/Core-i99-chrismas-theme", "zip", "./Build/Output/Temp/")
